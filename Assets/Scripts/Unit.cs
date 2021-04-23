102046a7b2f2ef5c300206201e7c1dcbc9bddfdb
mattcmerritt <mattcmerritt@gmail.com>
2021-04-23 00:27:38 -0400

    added template to create a unit given a prefab with an empty subclass script, modified UnitsToAdd so that it now takes these templates and builds the units

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public abstract class Unit : MonoBehaviour
 {
-    public char Initial;
-    public char Player;
+    public Sprite Sprite; 
+    public SpriteRenderer SpriteRenderer;
+    public Point Location, Target;
+    public char Direction; // direction that the unit is facing in
+    public int Health, Team;
+
+    private void Start() 
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = Sprite;
+    }
+
+    public virtual void UpdateTarget() 
+    {
+        // behavior to find target tile
+    }
+
+    public virtual void Act() 
+    {
+        // behavior to perform at end of turn
+    }
 
     public override string ToString()
     {
-        return "" + Initial + Player;
+        return "" + Team + Health;
     }
-
-    // this will be implemented later
 }
