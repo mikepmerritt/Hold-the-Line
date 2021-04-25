7c8d8a35b078bc8e6aa33533192ff7e4285b5f3a
mattcmerritt <mattcmerritt@gmail.com>
2021-04-24 21:59:50 -0400

    added turn counting and turn limits

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move
+{
+    // unimplemented, would be used in a stack to implement undoing moves
+}
